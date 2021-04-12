615b4704e984457d5b957b00c35456a84a267894
keryiing27 <81569360+keryiing27@users.noreply.github.com>
2021-04-12 21:22:26 +0800

    Update Main.java

--- LaptopSetterGetter/Main.java
+++ LaptopSetterGetter/Main.java
@@ -3,27 +3,34 @@ package LaptopSetterGetter;
 public class Main {
 
 	public static void main(String[] args) {
-		Laptop a = new Laptop();
+		Laptop acer = new Laptop();
 		
-		a.setBrand("Acer");
-		a.setDisplay("Full HD");
-		a.setStorage(4);
-		a.setProcessor("Intel i5");
-		a.setPrice(3269.99);
-		a.setUsername("Guest");
+		acer.setModel("AN515");
+		acer.setDisplay("Full HD");
+		acer.setStorage(4);
+		acer.setProcessor("Intel i5");
+		acer.setPrice(3269.99);
+		acer.setUsername("Guest");
+		acer.setDeleting("File.txt");
+		acer.setUpdating("intellij");
+		acer.setEndTask("Chrome");
 		//a.price = 10; // compile error, since price is private
 		//double p = a.getPrice(); // same as above
 		
 		System.out.println("LAPTOP PROPERTIES");
 		System.out.println("------------------------");
-		System.out.println("Brand :" + a.getBrand());
-		System.out.println("Display: " + a.getDisplay());
-		System.out.println("Storage: " + a.getStorage() + "GB RAM");
-		System.out.println("Processor: "+ a.getProcessor());
-		System.out.println("Price: RM " + a.getPrice());
+		System.out.println("Brand :" + acer.getModel());
+		System.out.println("Display: " + acer.getDisplay());
+		System.out.println("Storage: " + acer.getStorage() + "GB RAM");
+		System.out.println("Processor: "+ acer.getProcessor());
+		System.out.println("Price: RM " + acer.getPrice());
 		System.out.println("------------------------");
-		System.out.println("Username: " + a.getUsername());
 		
+		System.out.println("Username: " + acer.getUsername());
+		System.out.println("\nDeleting " + acer.getDeleting() + "......");
+		System.out.println("Updating " + acer.getUpdating() + "......");
+		System.out.println("Which program to end? : " + acer.getEndTask() + "\n");
+		acer.fullThrottle();
 		
 
 	}
